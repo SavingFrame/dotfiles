bc21a3e728c4bf718ee29751e22301125657cccc
Ivan <savingframe@gmail.com>
2025-08-04 23:10:31 +0200

    update envs

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixCats-nvim = {
+      url = "path:./software/nixCats-nvim";
+    };
+    opencode = {
+      url = "github:sst/opencode/v0.3.85";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, nixCats-nvim, opencode,  ... }@inputs: {
+    # replace 'joes-desktop' with your hostname here.
+    nixosConfigurations.nixosy = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs nixCats-nvim opencode; };
+      modules = [
+      ./configuration.nix 
+
+      home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.users.nixy = ./home.nix;
+        home-manager.extraSpecialArgs = { inherit inputs opencode; };
+
+        # Optionally, use home-manager.extraSpecialArgs to pass
+        # arguments to home.nix
+      }
+      {
+        nixpkgs.overlays = [
+          (final: prev: {
+            opencode = nixpkgs.legacyPackages.${prev.system}.opencode.overrideAttrs (old: rec {
+                version = "0.3.128";
+                 # `opencode` is a flake input
+                 src = opencode;
+                 node_modules = old.node_modules.overrideAttrs (nmOld: {
+                   outputHash = "sha256-ZtZvS0jF2YpkDeCdP2y1qX4fJVMq8BBq6EFwqvDEfdc=";
+                 });
+                 tui = old.tui.overrideAttrs (tuiOld: {
+                   # these two lines below are important
+                   src = src;
+                   modRoot = "packages/tui";
+                   vendorHash = "sha256-+j8+TjTzd7AH9Si9tS7noTpPcG1lz9j+tmxUTrPcThw=";
+                 });
+            });
+          })
+        ];
+      }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,101 @@
+{ config, pkgs, inputs, opencode, ... }:
+
+let
+grip-grab = pkgs.rustPlatform.buildRustPackage {
+  pname = "grip-grab";
+  version = "0.6.7";
+  
+  src = pkgs.fetchFromGitHub {
+    owner = "alexpasmantier";
+    repo = "grip-grab";
+    tag = "v0.6.7";
+    hash = "sha256-e7duLL4tjW+11jXUqU6sqoKTAPGkH81iDCfjtNcnd4I=";
+  };
+  
+  cargoHash = "sha256-i/wqlM4hoDPa9dmbSU5VVCYA4UdI5fI3EPadOj+/+LE=";
+};
+in {
+	imports = [
+	];
+  home.username = "nixy";
+  home.homeDirectory = "/home/nixy";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+
+  # Packages that should be installed to the user profile.
+
+  home.packages = [
+    pkgs.tree 
+    pkgs.kitty
+    pkgs.ghostty
+    pkgs.git
+    pkgs.fd
+    grip-grab
+    pkgs.lazygit
+  ];
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "Ivan";
+    userEmail = "savingframe@gmail.com";
+  };
+
+  # starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+  };
+  programs.fish.enable = true;
+
+    wayland.windowManager.hyprland.enable = true;
+    wayland.windowManager.hyprland.package = null;
+    wayland.windowManager.hyprland.portalPackage = null;
+    wayland.windowManager.hyprland.xwayland.enable = true;
+    wayland.windowManager.hyprland.settings = {
+      "$mod" = "SUPER";
+      bind = [
+      "$mod, B, exec, firefox"
+      "$mod, Q, exec, killactive"
+      "$mod, Return, exec, ghostty"
+      ];
+    };
+
+    home.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.05";
+}
