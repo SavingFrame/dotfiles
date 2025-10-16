242451f7ca87124a8c28c58d06467518869e17ce
SavingFrame <savingframe@gmail.com>
2025-10-16 22:59:05 +0200

    update dotfiles

---
+++ hypr/scripts/wallpaper_swww.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+TIMEOUT=720
+
+# Kill existing wallpaper processes
+for pid in $(pgrep -f wallpaper_swww.sh | grep -v $$); do
+    kill $pid
+done
+
+if ! [ -d ~/.config/hypr/wallpapers/ ]; then 
+    notify-send -t 5000 "~/.config/hypr/wallpapers does not exist" && exit 1
+fi
+
+if [ $(ls -1 ~/.config/hypr/wallpapers | wc -l) -lt 1 ]; then	
+    notify-send -t 9000 "The wallpaper folder is expected to have more than 1 image. Exiting Wallsetter." && exit 1
+fi
+
+while true; do
+    while [ "$WALLPAPER" == "$PREVIOUS" ]; do
+        WALLPAPER=$(find ~/.config/hypr/wallpapers/ -name '*' | awk '!/.git/' | tail -n +2 | shuf -n 1)
+    done
+
+    PREVIOUS=$WALLPAPER
+
+    swww img "$WALLPAPER" --transition-type random
+    wal -i $WALLPAPER
+    sleep $TIMEOUT
+done
